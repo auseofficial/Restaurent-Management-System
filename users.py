4322521fc3b93bfecdfa0530fdc4bef3400c24f7
Akib Us Suny Eshan <I-484@wgbd.com>
2024-11-25 12:11:57 +0600

    updated

---
+++ users.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+class User(ABC):
+    def __init__(self,name):
+        self.name=name
+        
+class Customer(User):
+    def __init__(self,name,money):
+        self.money=money
+        self.__order=order
+        super().__init__(name)
+        
+    @property
+    def order(self):
+        return self.__order
+    
+    
