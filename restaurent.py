b95520c2813000de0520931c1d20dd299a63b7c7
Akib Us Suny Eshan <I-484@wgbd.com>
2024-11-26 14:55:30 +0600

    updated

--- restaurent.py
+++ restaurent.py
@@ -1,7 +1,17 @@
-class Restaurent:
-    def __init__(self,name,chef,server,manager):
-        self.name=name
-        self.chef=chef
-        self.server=server
-        self.manager=manager
-        
+class Restaurant:
+    def __init__(self, name, chef, server, manager):
+        self.name = name
+        self.chef = chef
+        self.server = server
+        self.manager = manager
+        self.menu = []
+        self.revenue = 0
+        self.profit = 0
+
+    def add_employee(self, employee_type, employee):
+        if employee_type == 'chef':
+            self.chef = employee
+        elif employee_type == 'server':
+            self.server = employee  
+        elif employee_type == 'manager':
+            self.manager = employee  
