21aeda0b326d826a77d7fc0da68c80c2f49b9f5d
Akib Us Suny Eshan <I-484@wgbd.com>
2024-11-26 13:00:18 +0600

    updated

---
+++ restaurent.py
@@ -0,0 +1,7 @@
+class Restaurent:
+    def __init__(self,name,chef,server,manager):
+        self.name=name
+        self.chef=chef
+        self.server=server
+        self.manager=manager
+        
