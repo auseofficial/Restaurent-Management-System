ca96c8175f6f345ba25d172207a80a6b405d990b
Akib Us Suny Eshan <I-484@wgbd.com>
2024-11-27 12:36:19 +0600

    updated

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def main():
     menu.add_menu_item('pizza'.pizza_2)
     pizza_3=Pizza('meat pizza',600,'large',['meat','onion'])
     menu.add_menu_item('pizza'.pizza_3)
+    
+    burger_1=Burger('chicken burger',600,'large',['chicken','onion'])
+    menu.add_menu_item('burger',burger_1)
+    burger_2=Burger('beef burger',600,'large',['beef','onion'])
+    menu.add_menu_item('burger',burger_1)
+    
+    coke=
