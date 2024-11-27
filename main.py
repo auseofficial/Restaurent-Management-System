fb9be39187ec502a2f94f9d537438a5007f83db7
Akib Us Suny Eshan <I-484@wgbd.com>
2024-11-27 12:23:19 +0600

    updated

---
+++ main.py
@@ -0,0 +1,10 @@
+from menu import Pizza, Burger,Drinks,Menu
+
+def main():
+    menu=Menu()
+    pizza_1=Pizza('chicken pizza',600,'large',['chicken','onion'])
+    menu.add_menu_item('pizza'.pizza_1)
+    pizza_2=Pizza('cheese pizza',600,'large',['cheese','onion'])
+    menu.add_menu_item('pizza'.pizza_2)
+    pizza_3=Pizza('meat pizza',600,'large',['meat','onion'])
+    menu.add_menu_item('pizza'.pizza_3)
