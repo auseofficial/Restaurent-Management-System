b518f3d43afa049790cb21fb5adead2e93bcff2b
Akib Us Suny Eshan <I-484@wgbd.com>
2024-12-02 14:51:26 +0600

    updated

---
+++ test.py
@@ -0,0 +1,17 @@
+num1 = int(input('Enter first number: ')) 
+num2 = int(input("Enter second number: ")) 
+
+# Calculate the sum
+total = num1 + num2
+
+# Print the result
+print("The sum is:", total)
+
+
+num=input("Write a number :")
+num=int(num)
+
+if num%2==0:
+    print(num, "is even")
+else:
+    print(num,"is odd")
